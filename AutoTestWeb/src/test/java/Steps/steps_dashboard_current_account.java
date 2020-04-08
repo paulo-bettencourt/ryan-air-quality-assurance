dd03157c2f94ecdc66ea511aa1585d527f3c7ae7
anaritac30 <anarita31@sapo.pt>
2020-04-08 13:57:35 +0000

    PR testing

--- AutoTestWeb/src/test/java/Steps/steps_dashboard_current_account.java
+++ AutoTestWeb/src/test/java/Steps/steps_dashboard_current_account.java
@@ -21,7 +21,6 @@ public class steps_dashboard_current_account {
     public void dashboardIsLoaded() throws InterruptedException {
         Thread.sleep(3000);
         driver.findElement(By.xpath("//*[@id=\"bb-main-content\"]/bb-panel-container/bb-area/bb-chrome/bb-deck-container/bb-route/bb-chrome/bb-panel-container/bb-area/bb-chrome/bb-deck-container/bb-route/bb-chrome/bb-panel-container/bb-area/bb-chrome[3]/bb-column-container/div/bb-column[1]/bb-chrome/bb-panel-container/bb-area/bb-chrome/bb-panel-container/bb-area/bb-chrome/sba-sba-product-summary-list-widget-extended/bb-product-summary-list-widget/div[1]/div[2]"));
-        // driver.findElement(By.className("account-name"));
 
     }
 
