e382362ff0c85be520bba2471b03a865dab83a89
anaritac30 <anarita31@sapo.pt>
2020-06-26 13:32:49 +0000

    Debit and credit card details and transfers changes due to OTP

--- AutoTestWeb/src/test/java/Steps/steps_card_details_credit_card.java
+++ AutoTestWeb/src/test/java/Steps/steps_card_details_credit_card.java
@@ -17,10 +17,10 @@ public class steps_card_details_credit_card {
     }
 
     @Then("^i select one credit card$")
-    public void iSelectOneCreditCard() throws InterruptedException {
+    public void iSelectOneCreditCard() {
         driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         //click on the first credit card
         driver.findElement(By.xpath("/html/body/div/div/div/div/div/div/div/sba-sba-retail-app/bb-root/bb-area/bb-chrome/bb-layout-container/bb-layout-rendering-container-ui/bb-page-layout-theme1-ui/div/div[2]/div/div/div/bb-chrome[2]/bb-panel-container/bb-area/bb-chrome/bb-deck-container/bb-route/bb-chrome/bb-panel-container/bb-area/bb-chrome/bb-deck-container/bb-route/bb-chrome/bb-panel-container/bb-area/bb-chrome[3]/bb-column-container/div/bb-column[1]/bb-chrome/bb-panel-container/bb-area/bb-chrome/bb-panel-container/bb-area/bb-chrome/sba-sba-cards-list-widget-extended/bb-cards-list-widget/bb-payment-cards-container/bb-payment-card-group[1]/div/bb-payment-card/div/bb-payment-card-state-wrapper-ui/div")).click();
-Thread.sleep(5000);
+
     }
 }
