602c11539b2df73066d3f2831b9b302255c1d563
diogobrandao <diogo.brandao@bringglobal.com>
2020-11-16 11:25:43 -0100

    SharedDriverchange  - Wait driver fixed

--- AutoTestWeb/src/test/java/Steps/SharedDriver.java
+++ AutoTestWeb/src/test/java/Steps/SharedDriver.java
@@ -5,8 +5,10 @@ import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SharedDriver {
@@ -48,10 +50,14 @@ public class SharedDriver {
         }
     }
 
-    @Before
-    public static WebDriverWait getWait() {
+    public WebDriverWait getWait() {
         return wait;
     }
+
+    public void waitForElement(WebElement element) {
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+    
 
     public WebDriver getDriver() {
         return driver;
