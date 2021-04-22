08bdbcd7b38447902d6d1cc47236798dc2389857
diogobrandao <diogo.brandao@bringglobal.com>
2021-04-22 16:14:01 +0000

    Fixed all dashboard tests business

--- AutoTestWeb/src/test/java/Actions/Business_Dashboard.java
+++ AutoTestWeb/src/test/java/Actions/Business_Dashboard.java
@@ -99,7 +99,6 @@ public class Business_Dashboard {
 
     @FindBy(how = How.XPATH, using = "//h2[contains(text(), ' Últimas Transacções ')]")
     WebElement dashboardLatestTransactionsTitle;
-
     public void iAmInBusinessDashboard() {
         sharedDriver.getWait().until(ExpectedConditions.visibilityOf(dashboardTitle));
     }
