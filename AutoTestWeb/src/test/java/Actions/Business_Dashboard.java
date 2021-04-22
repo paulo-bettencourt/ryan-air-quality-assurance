5538ba2defabedf5114ec2ac6d1e4b53585fa680
diogobrandao <diogo.brandao@bringglobal.com>
2021-04-22 16:42:45 +0000

    Fixed all dashboard tests business

--- AutoTestWeb/src/test/java/Actions/Business_Dashboard.java
+++ AutoTestWeb/src/test/java/Actions/Business_Dashboard.java
@@ -19,9 +19,6 @@ public class Business_Dashboard {
         PageFactory.initElements(sharedDriver.getDriver(), this);
     }
 
-    @FindBy(how = How.XPATH, using = "//span[contains(text(), 'Home' )]")
-    WebElement sideNavigationBarTitle;
-
     @FindBy(how = How.XPATH, using = "//*[@id=\"context-menu-dropdown\"]/bb-dropdown-menu-ui//div[contains(text(), 'Hi')] | //*[@id=\"context-menu-dropdown\"]/bb-dropdown-menu-ui//div[contains(text(), 'Olá')]")
     WebElement helloMessage;
 
@@ -93,13 +90,6 @@ public class Business_Dashboard {
 
     @FindBy(how = How.XPATH, using = "//*[@id=\"e49fd830-de81-4e49-b54f-39ff13a230fb\"]/sba-sba-business-app/bb-root/bb-area/bb-chrome//table/tbody/tr[1]/td[8]/bb-dropdown-menu-ui/div/button/div")
     WebElement LettersOfCreditKebabMenu;
-
-
-    @FindBy(how = How.XPATH, using = "//h2[contains(text(), ' Contas Favoritas ')]")
-    WebElement dashboardFavoriteTitle;
-
-    @FindBy(how = How.XPATH, using = "//h2[contains(text(), ' Últimas Transacções ')]")
-    WebElement dashboardLatestTransactionsTitle;
 
     public void iAmInBusinessDashboard() {
         sharedDriver.getWait().until(ExpectedConditions.visibilityOf(helloMessage));
@@ -200,13 +190,5 @@ public class Business_Dashboard {
     public void iChooseBankGuaranteeKebabMenuDetails() {
         sharedDriver.getWait().until(ExpectedConditions.visibilityOf(BankGuaranteeDetailsButton)).click();
         sharedDriver.getWait().until(ExpectedConditions.visibilityOf(generalLabelDetails));
-    }
-
-    public void iSeeFavAccounts() {
-        sharedDriver.getWait().until(ExpectedConditions.visibilityOf(dashboardFavoriteTitle));
-    }
-
-    public void iSeeLatestTransactions(){
-        sharedDriver.getWait().until(ExpectedConditions.visibilityOf(dashboardLatestTransactionsTitle));
     }
 }
